9c0c92281ccc8d1f770d291532e5bcce856d34c2
Ruben Perez <ruben.perezjuarez@mail.mcgill.ca>
2023-05-02 00:45:11 -0400

    added force-dynamic to fix deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Container from "./components/Container";
 import ListingCard from "./components/listings/ListingCard";
 import EmptyState from "./components/EmptyState";
